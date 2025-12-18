2b1aa4b731cf72d3ca79d7bd1fc428612fa4822e
sridharanselvarajan <sridharan.s2023ai-ds@gmail.com>
2025-12-18 21:26:27 +0530

    cors allow

--- src/index.js
+++ src/index.js
@@ -40,7 +40,7 @@ const PORT = process.env.PORT || 5000;
 // middlewares
 app.use(
   cors({
-    origin: ['http://localhost:5173','https://book-frontend-dvln.vercel.app'],
+    origin: ['http://localhost:5173','https://book-frontend-nrge.vercel.app'],
     credentials: true,
   })
 );
